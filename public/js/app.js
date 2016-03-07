efa84003fcd1896fbe064874c1a2639adf08a848
Andrew Lloyd <MissedMemo@GMail.com>
2016-03-07 07:45:53 -0800

    add basic uiRouter view switching support

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-var app = angular.module( 'app', [] );
+var app = angular.module( 'app', ['ui.router'] );
+
+app.config( function( $stateProvider, $urlRouterProvider ) {
+    
+  $urlRouterProvider.otherwise('/home');
+    
+  $stateProvider
+        
+    .state('home', {
+      url: '/home',
+      templateUrl: '../views/home.html'
+    })
+        
+    .state('companies', {
+      url: '/companies',
+      templateUrl: '../views/companies.html'      
+    });
+        
+});
