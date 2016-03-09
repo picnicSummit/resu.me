fee3c697a89e6f333f696c4209f08c80a9bbefaa
Kathleen L <kathleeng.lopez@gmail.com>
2016-03-08 22:13:39 -0800

    Updated company schema, working on editing feature

--- app/models/company.js
+++ app/models/company.js
@@ -2,7 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var companySchema = new Schema({
-  name: String
+	
+  name: String,
+  status: {
+    applied: Boolean,
+    phone: Boolean,
+    onsite: Boolean,
+    offer: Boolean,
+    accepted: Boolean 
+  }
+
 });
 
 module.exports = mongoose.model( 'Company', companySchema );
