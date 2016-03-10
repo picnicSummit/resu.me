d0b635733a138c890cf07ead6cbc50792199a30a
Colin McDonald <mickydwow@gmail.com>
2016-03-09 18:36:41 -0800

    Got the on-load view working.

--- public/js/controllers/navbar-controller.js
+++ public/js/controllers/navbar-controller.js
@@ -2,10 +2,12 @@ angular.module('app')
   .controller( 'NavbarController', ['$scope', '$state', function( $scope, $state ) {
 
     $scope.setCompany = function() {
-      $state.transitionTo('view');
+      $state.go('companies.view');
     };
 
     $scope.setCalendar = function() {
-      $state.transitionTo('calendar');
+      $state.go('companies.calendar');
     };
+
+    $scope.setCompany();
   }]);
