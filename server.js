11617dd99499eb1b9063ae6f76b6eceed7a220f1
Colin McDonald <mickydwow@gmail.com>
2016-03-13 01:20:27 -0800

    Edited URI

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 var port = process.env.PORT || 4568;
-var dbURI = 'mongodb://localhost/jobhunt';
+var dbURI = 'mongodb://heroku_qbvl1qx6:u764ejr6tc2u04ht5lms87vdm0@ds011389.mlab.com:11389/heroku_qbvl1qx6';
 
 
 mongoose.connect( dbURI );
