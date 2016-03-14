f5c070fb627aa4f94e824143894a3748d57c6d26
Colin McDonald <mickydwow@gmail.com>
2016-03-14 16:08:24 -0700

    Dates are now full kat approved

--- public/js/controllers/date-controller.js
+++ public/js/controllers/date-controller.js
@@ -6,27 +6,33 @@ angular.module('app')
     $scope.$watch('selection', function() {
       $scope.phoneDate = null;
       $scope.onsiteDate = null;
+      $scope.phoneCheck = false;
+      $scope.onsiteCheck = false;
 
       if ($scope.selection.dates && $scope.selection.dates.phone) {
         var phone = new Date($scope.selection.dates.phone);
         $scope.phoneDate = phone;
+        $scope.phoneCheck = true;
       }
 
       if ($scope.selection.dates && $scope.selection.dates.onsite) {
         var onsite = new Date($scope.selection.dates.onsite);
         $scope.onsiteDate = onsite;
+        $scope.onsiteCheck = true;
       }
     });
 
     $scope.setPhoneDate = function(date) {
       CompanyFactory.setPhoneDate(date, $scope.selection._id, 'phone', function() {
         $scope.getAll($scope.index);
+        $scope.phoneCheck = true;
       });
     };
 
     $scope.setOnsiteDate = function(date) {
       CompanyFactory.setOnsiteDate(date, $scope.selection._id, 'onsite');
       $scope.getAll($scope.index);
+      $scope.onsiteCheck = true;
     };
 
   }]);
