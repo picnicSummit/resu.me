b4c7623da3d20c0886cdf874750622f745695038
Colin McDonald <mickydwow@gmail.com>
2016-03-14 11:01:46 -0700

    Working on getting things working

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-var port = process.env.PORT || 4568;
+var port = process.env.PORT || 4500;
 var dbURI = 'mongodb://heroku_qbvl1qx6:u764ejr6tc2u04ht5lms87vdm0@ds011389.mlab.com:11389/heroku_qbvl1qx6';
 
 
