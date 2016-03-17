bb64b6a14d9d40d55efc07372f81ad52188cfda9
DejonGill <dejonjgill@gmail.com>
2016-03-17 14:48:55 -0700

    Added rich text editor to tasks-coverletter.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module( 'app', ['ui.router', 'ui.bootstrap'] );
+var app = angular.module( 'app', ['ui.router', 'ui.bootstrap', 'textAngular'] );
 
 app.config( function( $stateProvider, $urlRouterProvider ) {
     
